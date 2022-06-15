22b5843074f8991a88e2eab291f6e96241b0cb44
DESKTOP-G53QRMN\marka <markabdilla26@gmail.com>
2022-06-15 20:19:11 +0200

    Project setup and array population

---
+++ README.md
@@ -0,0 +1,15 @@
+**Outline of Project**
+
+Create arrays for different types of strings:
+
+1. Character Names
+2. Infliction Types
+3. Weapon
+4. Weapon action
+5. Phrase
+
+The arrays will be populated manually
+
+3 messages are to be logged to the console.
+
+Messages will be made up of randomly selected strings from the different arrays, based on preset patterns.
---
+++ app.js
@@ -0,0 +1,6 @@
+let names = ["Godrick the Grafted", "Rennala, Queen of Caria", "Starscourge Radhan", "Praetor Rykard, Lord of Blasphemy", "Morgott, Omen King", "Malekith, the Black Blade", "Radagon of the Golden Order", "Malenia, Blade of Miquella", "Mohg, Lord of Blood"];
+let inflictions = ["bleed", "poison", "scarlet rot", "magic spam", "join the family", "fall to Destined Death", "get crushed by a meteorite", "burnt to a crisp"];
+let weapons = ["Great Axe", "Carian Staff", "Starscourge Sword", "Big Snake Sword", "Cool Omen Blade", "Badass Death Sword", "Hammer that broke Elden Ring", "Unfair Appendage of Rot", "Nihil Blood"];
+let weaponActions = ["crush mightily", "spews magic", "create gravitational blast", "stab you into explosion", "slash into ghost hammer", "just kill you", "smash you to bits", "murder you while healing them", "remove all your blood"];
+let phrases = ["Lowly Tarnished. Thou are unfit even to graft.", "Come my sweetlings. Time to be reborn anew.", "*howls at sky incoherently*", "TOGETHA WE WILL DEVOUA THA VERY GODS", "Thy part in this shall not be forgiven.", "I will not have it stolen from me again.", "*stares at you in silence*", "I am Malenia. Blade of Miquella.", "Miquella is mine and mine alone."];
+
